0fa5f7290148ee4c2bee6a9156b1990779ebeb6d
ShawnFrancia <159972209+ShawnFrancia@users.noreply.github.com>
2025-01-28 20:30:04 -0700

    Week 2 assingment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Shawn Francia</h1>
+    <Link href="https://github.com/ShawnFrancia/cprg306-assignments">https://github.com</Link>
+    </div>
+  );
+}
